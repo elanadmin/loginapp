d4c29fa47d9a366e4a2a3c52a82fe8e2d451e533
elanadmin <rajesh.moturi@elantecs.com>
2017-07-05 07:00:36 -0500

    Changed script deploywar.sh

--- scripts/deploywar.sh
+++ scripts/deploywar.sh
@@ -6,4 +6,8 @@ apt-get install -y tomcat7 lsof
 service tomcat7 start
 update-rc.d tomcat7 enable
 
-[ -f /var/lib/tomcat7/webapps/loginapp.war ] && rm -rf /var/lib/tomcat7/webapps/loginapp.war
+if [ -f /var/lib/tomcat7/webapps/loginapp.war ];then
+rm -rf /var/lib/tomcat7/webapps/loginapp.war
+fi
+
+exit 0
