befcf9cbbd6da6aec8cad15b36d36fecf67d69df
elanadmin <rajesh.moturi@elantecs.com>
2017-07-15 00:50:21 -0500

    Added mysql table creation.

---
+++ scripts/mysql_add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install -y mysql-client
+
+if [ -f /root/rds_servers ];then
+for i in $(cat /root/rds_servers)
+do
+mysql -h $server loginapp < loginapp.users.sql
+done
+fi
+
+exit 0
