2160d99e80390e0315577058422ff57b674e84c8
elanadmin <rajesh.moturi@elantecs.com>
2017-07-15 01:09:59 -0500

    Modified to create mysql table.

--- scripts/mysql_add.sh
+++ scripts/mysql_add.sh
@@ -6,7 +6,7 @@ apt-get install -y mysql-client
 if [ -f /root/rds_servers ];then
 for i in $(cat /root/rds_servers)
 do
-mysql -h $server loginapp < loginapp.users.sql
+mysql -h $server loginapp < /root/loginapp.users.sql
 done
 fi
 
