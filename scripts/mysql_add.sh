e5efcfbb96e19e18f89adcd4d4a9357521dfa42d
elanadmin <rajesh.moturi@elantecs.com>
2017-07-15 01:29:08 -0500

    modified mysql_add.sh

--- scripts/mysql_add.sh
+++ scripts/mysql_add.sh
@@ -4,9 +4,9 @@ apt-get update -y
 apt-get install -y mysql-client
 
 if [ -f /root/rds_servers ];then
-for i in $(cat /root/rds_servers)
+for rds in $(cat /root/rds_servers)
 do
-mysql -h $server loginapp < /root/loginapp.users.sql
+mysql -h $rds loginapp < /root/loginapp.users.sql
 done
 fi
 
